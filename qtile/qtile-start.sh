67f0a393d52c93e790691f1b2172266f93c5e15f
Dirk Moeller <moeller.dirk80@gmail.com>
2025-04-03 08:42:18 +0200

    initial

---
+++ qtile/qtile-start.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+#export GDK_SCALE=2
+#export GDK_DPI_SCALE=0.5
+
+#xrandr --dpi 160
+BIG=DisplayPort-0
+SMALL=DisplayPort-1
+xrandr --output ${BIG} --primary --left-of ${SMALL} --pos 0x0
+xrandr --output ${SMALL} --pos 3840x1080 --scale 2x2
+xrandr --output ${SMALL} --brightness 0.5
+
+exec qtile start
