e5e9ba2a3041fa8be23ce5e2afa305e11676ac54
Dirk Moeller <moeller.dirk80@gmail.com>
2025-04-23 10:41:55 +0200

    improve monitor scripts

--- scripts/monitor-single.sh
+++ scripts/monitor-single.sh
@@ -2,5 +2,12 @@
 
 MON1=DisplayPort-0
 MON2=DisplayPort-1
+
+if [ "$HOSTNAME" == dl* ]; then
+    MON1=HDMI-2
+    MON2=HDMI-3
+fi
+
 xrandr --output $MON2 --off
 
+notify-send "Single monitor setup: Using $MON1."
